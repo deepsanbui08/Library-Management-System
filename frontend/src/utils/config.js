3d162a5fb59d7616b259c3b916425177a187afb7
Coder <deep.sanbui2004@gmail.com>
2026-03-21 13:11:17 +0530

    Update Server_URL to use environment variable

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,2 @@
 //export const Server_URL  = 'http://localhost:5000/'
-export const Server_URL  = 'https://library-management-system-backend-cv0i.onrender.com'
+export const Server_URL  = import.meta.env.VITE_BACKEND_URL
